9418e28137be6441ca739ab3db418b07c2cbd514
DevReaxul <hreazul11@gmail.com>
2024-03-01 23:55:15 +0600

    Add login form and authentication logic

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,32 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
- return (
-  <div>
-     <h2>This is Login component</h2>
-  </div>
- );
+   const { register, handleSubmit } = useForm();
+  const [Login, { data, error }] = useLoginMutation();
+  console.log({data, error});
+
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    }
+    Login(userInfo);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input {...register('id')} type="text" title="id" id="id" />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input {...register('password')} type="text" title="password" id="password" />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
 };
 
 export default Login;
